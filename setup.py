11ec66a305e8df6c70ef4b5ddf3ef4758fe23ea7
Self-nasu <124767257+Self-nasu@users.noreply.github.com>
2024-05-14 19:02:56 +0530

    authorname changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "eazyAI",
     version = '1.1.03',
     packages = find_packages(),
-    author = 'Puneet Dhankar',
+    author = 'Puneet Dhankar , Naman Jain',
     author_email = "puneetd2005@gmail.com",
     description = 'A one function go library for Ski-Kit learn and TensorFlow AI/ML models build-in.',
     url = 'https://github.com/Self-nasu/eazyAI',
