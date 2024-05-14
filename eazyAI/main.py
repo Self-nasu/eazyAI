2eda22101ada28ff1009bb28123c355ce8293f32
Self-nasu <124767257+Self-nasu@users.noreply.github.com>
2024-05-14 16:12:27 +0530

    Data Gen

--- eazyAI/main.py
+++ eazyAI/main.py
@@ -2,6 +2,9 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import inspect
 import tensorflow as tf
+import pandas as pd
+import numpy as np
+
 
 def just_plot(
         Data, 
