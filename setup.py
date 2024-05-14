911d858171e0172c591b40f338df63e2ed8b6779
Self-nasu <124767257+Self-nasu@users.noreply.github.com>
2024-05-14 19:02:19 +0530

    Changes Done

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author = 'Puneet Dhankar',
     author_email = "puneetd2005@gmail.com",
     description = 'A one function go library for Ski-Kit learn and TensorFlow AI/ML models build-in.',
-    url = 'https://github.com/self-Puneet/eazyAI',
+    url = 'https://github.com/Self-nasu/eazyAI',
     install_requires=[
         'numpy',
         'pandas',
