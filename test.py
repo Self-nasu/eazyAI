770f2ff4fde46e98e9a662f8d6686fda73c90278
self-Puneet <148076373+self-Puneet@users.noreply.github.com>
2024-05-16 12:35:34 +0530

    added just_plo funtion to plot when X, Y dataframes are passed seperatly. changed the version too. not changed init file as you told me.

--- test.py
+++ test.py
@@ -17,46 +17,28 @@ def just_plot(
         title = None,
         ):
     
-    # for i in range(0,len(Data)):
-    #     calling_frame = inspect.currentframe().f_back
-    #     variable_name = [var_name for var_name, var_value in calling_frame.f_locals.items() if var_value is Data[i]][0]
-    #     Label_names.append(variable_name)
-    
-
-    # 
-    if len(X) > 1:
+    Y_Label_names = Y.name
+    if len(X.shape) > 1:
         X_Label_names = list(X.columns)
-    X_Label_names = X.columns
+    else:
+        X_Label_names = X.name
 
-    calling_frame = inspect.currentframe().f_back
-    Y_Label_names = [var_name for var_name, var_value in calling_frame.f_locals.items() if var_value is Y][0]     
+    print(X)
 
-
-    if isinstance(X, pd.DataFrame) is True:
-        Y = Y.values
-    Y = np.transpose(Y)
-
-
-
-
-
-
-
-    if (len(X) == 1):
+    if X.shape[1] == 1:
         plt.scatter(X, Y, c = color, s = size, marker= shape)
         plt.savefig(save) if save else None
         plt.title(title)
         plt.xlabel(X_Label_names[0]) 
         plt.ylabel(Y_Label_names[0])
         plt.show()
-            
-        
-    elif (len(X) == 2):
+                  
+    elif X.shape[1] == 2:
         figure = plt.figure()
         axis = figure.add_subplot(111, projection = '3d')
-        if (size == None): 
+        if size == None: 
             size = 20 
-        axis.scatter(X[0], X[1], Y[0], s= size, c = color, marker=shape)
+        axis.scatter(X[X.columns[0]], X[X.columns[1]], Y, s= size, c = color, marker = shape)
         plt.savefig(save) if save else None
         plt.title(title)
         axis.set_xlabel(X_Label_names[0])
@@ -64,21 +46,22 @@ def just_plot(
         axis.set_zlabel(Y_Label_names[0])
         plt.show()
 
-    elif (len(X) == 3):  
+    elif X.shape[1] == 3:  
         figure = plt.figure()
         axis = figure.add_subplot(111, projection = '3d')
         if size == None: 
             size = 20 
-        scatter = axis.scatter(X[0], X[1], X[2], s = size ,c = Y[0], cmap = 'plasma')
+        scatter = axis.scatter(X[X.columns[0]], X[X.columns[1]], X[X.columns[2]], s = size, marker = shape, c = Y, cmap = 'plasma')
         plt.colorbar(scatter, label=Y_Label_names[0])
         plt.savefig(save) if save else None
         axis.set_xlabel(X_Label_names[0])
         axis.set_ylabel(X_Label_names[1])
         axis.set_zlabel(X_Label_names[2])
         if title != None:
-            axis.title(title)                                                                                                                    
+            plt.title(title)                                                                                                                    
         plt.show()
 
     else:
         print("Warning: Unable to plot more than 4 Dimension.")
+
 
