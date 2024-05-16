8f878fca77b81862e315a1ee1fc387d5de4cf626
Self-nasu <124767257+Self-nasu@users.noreply.github.com>
2024-05-16 23:01:51 +0530

    Version 1.1.6 Live Now

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# eazyAI
-The one function go library for making sklearn and tensorflow model in ezy way.
+
+# EazyAi 1.1.6
+
+A one-function Go library designed to simplify the creation of AI/ML models using scikit-learn and TensorFlow. This library also includes several helpful utility functions to assist in various tasks.
+
+## Authors
+
+- [@Self-nasu](https://github.com/Self-nasu)
+- [@Self-puneet](https://github.com/self-puneet)
+
+## Installation
+
+To install the EazyAi library, use the following command:
+
+```bash
+pip install eazyAI
+```
+
+## Function Details
+
+The EazyAi library provides several convenient functions. Below are the details on how to import them:
+
+```python
+from eazyAI import just_plot_dataframe
+from eazyAI import ezhelp
+from eazyAI import gen_data
+from eazyAI import just_plot_csv
+from eazyAI import just_plot
+```
+
+---
--- eazyAI/main.py
+++ eazyAI/main.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-version = "1.1.5"
+version = "1.1.6"
 
 # +++++++++++++++++++++++++++++++++++++ MAIN HELP FUNCTION ++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -26,6 +26,7 @@ def ezhelp():
     print("2. just_plot_dataframe()")
     print("3. just_plot_csv()")
     print("4. gen_data()")
+    print("4. just_plot()")
 
 # ==================================== DATA SET GENREATOR ==================================================
 
@@ -202,6 +203,66 @@ def just_plot_csv(
         axis.set_zlabel(Label_names[2])
         if title != None:
             axis.title(title)                                                                                                                    
+        plt.show()
+
+    else:
+        print("Warning: Unable to plot more than 4 Dimension.")
+
+
+# ==================================== JUST PLOT divied x,y data frames ==================================================
+
+def just_plot(
+        X,
+        Y,
+        size = None,
+        color = None,
+        shape = None,
+        save = None,
+        title = None,
+        ):
+    
+    Y_Label_names = Y.name
+    if len(X.shape) > 1:
+        X_Label_names = list(X.columns)
+    else:
+        X_Label_names = X.name
+
+    print(X)
+
+    if X.shape[1] == 1:
+        plt.scatter(X, Y, c = color, s = size, marker= shape)
+        plt.savefig(save) if save else None
+        plt.title(title)
+        plt.xlabel(X_Label_names[0]) 
+        plt.ylabel(Y_Label_names[0])
+        plt.show()
+                  
+    elif X.shape[1] == 2:
+        figure = plt.figure()
+        axis = figure.add_subplot(111, projection = '3d')
+        if size == None: 
+            size = 20 
+        axis.scatter(X[X.columns[0]], X[X.columns[1]], Y, s= size, c = color, marker = shape)
+        plt.savefig(save) if save else None
+        plt.title(title)
+        axis.set_xlabel(X_Label_names[0])
+        axis.set_ylabel(X_Label_names[1])
+        axis.set_zlabel(Y_Label_names[0])
+        plt.show()
+
+    elif X.shape[1] == 3:  
+        figure = plt.figure()
+        axis = figure.add_subplot(111, projection = '3d')
+        if size == None: 
+            size = 20 
+        scatter = axis.scatter(X[X.columns[0]], X[X.columns[1]], X[X.columns[2]], s = size, marker = shape, c = Y, cmap = 'plasma')
+        plt.colorbar(scatter, label=Y_Label_names[0])
+        plt.savefig(save) if save else None
+        axis.set_xlabel(X_Label_names[0])
+        axis.set_ylabel(X_Label_names[1])
+        axis.set_zlabel(X_Label_names[2])
+        if title != None:
+            plt.title(title)                                                                                                                    
         plt.show()
 
     else:
